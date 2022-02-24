79fc88b563d1c5bd12155bb002b0371f9f6ef548
Son Le <sonltcoder@gmail.com>
2022-02-25 04:01:30 +0700

    Testing OK

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/ethereum/go-ethereum v1.10.3
 	github.com/fatih/color v1.13.0
+	github.com/logdna/logdna-go v1.0.2
 )
--- log/default.go
+++ log/default.go
@@ -1,201 +1,190 @@
 package log
 
-import (
-	"fmt"
-	"sync"
-	"time"
+import "fmt"
 
-	"github.com/fatih/color"
-)
-
-// Ensure struct implements interface at compile-time
-var _ Logger = (*DefaultLogger)(nil)
-
-type Logger interface {
-	Critical(a ...interface{})
-	Error(a ...interface{})
-	Warn(a ...interface{})
-	Info(a ...interface{})
-	Verbose(a ...interface{})
-	Debug(a ...interface{})
-	Errorf(template string, a ...interface{})
-	Warnf(template string, a ...interface{})
-	Infof(template string, a ...interface{})
-	Debugf(template string, a ...interface{})
-	Verbosef(template string, a ...interface{})
-}
-
-const (
-	LOG_LEVEL_DEBUG = iota + 1
-	LOG_LEVEL_VERBOSE
-	LOG_LEVEL_INFO
-	LOG_LEVEL_WARN
-	LOG_LEVEL_ERROR
-	LOG_LEVEL_CRITICAL
-)
-
-var globalLogger Logger
-var logLevel int
-var loggerLock = &sync.RWMutex{}
-
-func SetLogger(logger Logger) {
-	loggerLock.Lock()
-	defer loggerLock.Unlock()
-
-	globalLogger = logger
-}
-
-func SetLogLevel(level int) {
-	loggerLock.Lock()
-	defer loggerLock.Unlock()
-
-	logLevel = level
-}
-
-func getLogger() Logger {
-	loggerLock.RLock()
-
-	if globalLogger == nil {
-		// Release the lock so that it could be locked again in the set functions
-		loggerLock.RUnlock()
-
-		SetLogger(newDefaultLogger())
-		SetLogLevel(LOG_LEVEL_INFO)
-	} else {
-		loggerLock.RUnlock()
-	}
-
-	return globalLogger
-}
-
-func Info(a ...interface{}) {
-	logger := getLogger()
-	logger.Info(a...)
-}
-
-func Debug(a ...interface{}) {
-	logger := getLogger()
-	logger.Debug(a...)
-}
-
-func Warn(a ...interface{}) {
-	logger := getLogger()
-	logger.Warn(a...)
-}
-
-func Error(a ...interface{}) {
-	logger := getLogger()
-	logger.Error(a...)
-}
-
-func Verbose(a ...interface{}) {
-	logger := getLogger()
-	logger.Verbose(a...)
-}
-
-func Critical(a ...interface{}) {
-	logger := getLogger()
-	logger.Critical(a...)
-}
-
-func Infof(template string, a ...interface{}) {
-	logger := getLogger()
-	logger.Infof(template, a...)
-}
-
-func Debugf(template string, a ...interface{}) {
-	logger := getLogger()
-	logger.Debugf(template, a...)
-}
-
-func Warnf(template string, a ...interface{}) {
-	logger := getLogger()
-	logger.Warnf(template, a...)
-}
-
-func Errorf(template string, a ...interface{}) {
-	logger := getLogger()
-	logger.Errorf(template, a...)
-}
-
-func Verbosef(template string, a ...interface{}) {
-	logger := getLogger()
-	logger.Verbosef(template, a...)
-}
-
-// Default logging implementation. You can replace this logging module by another implementation
-// of Logger interface.
-
-type DefaultLogger struct {
-	errorColor   *color.Color
-	verboseColor *color.Color
-	warningColor *color.Color
-	debugColor   *color.Color
-}
-
-func newDefaultLogger() *DefaultLogger {
-	return &DefaultLogger{
-		errorColor:   color.New(color.FgRed),
-		verboseColor: color.New(color.FgCyan),
-		warningColor: color.New(color.FgYellow),
-		debugColor:   color.New(color.FgMagenta),
-	}
-}
-
-func (logger *DefaultLogger) Info(a ...interface{}) {
-	logger.printWithTime(nil, "", a...)
-}
-
-func (logger *DefaultLogger) Debug(a ...interface{}) {
-	logger.printWithTime(logger.debugColor, "", a...)
-}
-
-func (logger *DefaultLogger) Warn(a ...interface{}) {
-	logger.printWithTime(logger.warningColor, "", a...)
-}
-
-func (logger *DefaultLogger) Error(a ...interface{}) {
-	logger.printWithTime(logger.errorColor, "", a...)
-}
-
-func (logger *DefaultLogger) Verbose(a ...interface{}) {
-	logger.printWithTime(logger.verboseColor, "", a...)
-}
-
-func (logger *DefaultLogger) Critical(a ...interface{}) {
-	logger.printWithTime(nil, "", a...)
-}
-
-func (logger *DefaultLogger) Infof(template string, a ...interface{}) {
-	logger.printWithTime(nil, template, a...)
-}
-
-func (logger *DefaultLogger) Debugf(template string, a ...interface{}) {
-	logger.printWithTime(logger.debugColor, template, a...)
-}
-
-func (logger *DefaultLogger) Warnf(template string, a ...interface{}) {
-	logger.printWithTime(logger.warningColor, template, a...)
-}
-
-func (logger *DefaultLogger) Errorf(template string, a ...interface{}) {
-	logger.printWithTime(logger.errorColor, template, a...)
-}
-
-func (logger *DefaultLogger) Verbosef(template string, a ...interface{}) {
-	logger.printWithTime(logger.verboseColor, template, a...)
-}
-
-func (logger *DefaultLogger) printWithTime(c *color.Color, template string, a ...interface{}) {
-	now := time.Now().Format("15:04:05.00")
-	msg := getMessage(template, a...)
-	if c == nil {
-		fmt.Println(now, msg)
-	} else {
-		c.Println(now, msg)
-	}
-}
-
+//
+//import (
+//	"fmt"
+//	"sync"
+//	"time"
+//
+//	"github.com/fatih/color"
+//)
+//
+//// Ensure struct implements interface at compile-time
+//var _ Logger = (*DefaultLogger)(nil)
+//
+//const (
+//	LOG_LEVEL_DEBUG = iota + 1
+//	LOG_LEVEL_VERBOSE
+//	LOG_LEVEL_INFO
+//	LOG_LEVEL_WARN
+//	LOG_LEVEL_ERROR
+//	LOG_LEVEL_CRITICAL
+//)
+//
+//var globalLogger Logger
+//var logLevel int
+//var loggerLock = &sync.RWMutex{}
+//
+//func SetLogger(logger Logger) {
+//	loggerLock.Lock()
+//	defer loggerLock.Unlock()
+//
+//	globalLogger = logger
+//}
+//
+//func SetLogLevel(level int) {
+//	loggerLock.Lock()
+//	defer loggerLock.Unlock()
+//
+//	logLevel = level
+//}
+//
+//func getLogger() Logger {
+//	loggerLock.RLock()
+//
+//	if globalLogger == nil {
+//		// Release the lock so that it could be locked again in the set functions
+//		loggerLock.RUnlock()
+//
+//		SetLogger(newDefaultLogger())
+//		SetLogLevel(LOG_LEVEL_INFO)
+//	} else {
+//		loggerLock.RUnlock()
+//	}
+//
+//	return globalLogger
+//}
+//
+//func Info(a ...interface{}) {
+//	logger := getLogger()
+//	logger.Info(a...)
+//}
+//
+//func Debug(a ...interface{}) {
+//	logger := getLogger()
+//	logger.Debug(a...)
+//}
+//
+//func Warn(a ...interface{}) {
+//	logger := getLogger()
+//	logger.Warn(a...)
+//}
+//
+//func Error(a ...interface{}) {
+//	logger := getLogger()
+//	logger.Error(a...)
+//}
+//
+//func Verbose(a ...interface{}) {
+//	logger := getLogger()
+//	logger.Verbose(a...)
+//}
+//
+//func Critical(a ...interface{}) {
+//	logger := getLogger()
+//	logger.Critical(a...)
+//}
+//
+//func Infof(template string, a ...interface{}) {
+//	logger := getLogger()
+//	logger.Infof(template, a...)
+//}
+//
+//func Debugf(template string, a ...interface{}) {
+//	logger := getLogger()
+//	logger.Debugf(template, a...)
+//}
+//
+//func Warnf(template string, a ...interface{}) {
+//	logger := getLogger()
+//	logger.Warnf(template, a...)
+//}
+//
+//func Errorf(template string, a ...interface{}) {
+//	logger := getLogger()
+//	logger.Errorf(template, a...)
+//}
+//
+//func Verbosef(template string, a ...interface{}) {
+//	logger := getLogger()
+//	logger.Verbosef(template, a...)
+//}
+//
+//// Default logging implementation. You can replace this logging module by another implementation
+//// of Logger interface.
+//
+//type DefaultLogger struct {
+//	errorColor   *color.Color
+//	verboseColor *color.Color
+//	warningColor *color.Color
+//	debugColor   *color.Color
+//}
+//
+//func newDefaultLogger() *DefaultLogger {
+//	return &DefaultLogger{
+//		errorColor:   color.New(color.FgRed),
+//		verboseColor: color.New(color.FgCyan),
+//		warningColor: color.New(color.FgYellow),
+//		debugColor:   color.New(color.FgMagenta),
+//	}
+//}
+//
+//func (logger *DefaultLogger) Info(a ...interface{}) {
+//	logger.printWithTime(nil, "", a...)
+//}
+//
+//func (logger *DefaultLogger) Debug(a ...interface{}) {
+//	logger.printWithTime(logger.debugColor, "", a...)
+//}
+//
+//func (logger *DefaultLogger) Warn(a ...interface{}) {
+//	logger.printWithTime(logger.warningColor, "", a...)
+//}
+//
+//func (logger *DefaultLogger) Error(a ...interface{}) {
+//	logger.printWithTime(logger.errorColor, "", a...)
+//}
+//
+//func (logger *DefaultLogger) Verbose(a ...interface{}) {
+//	logger.printWithTime(logger.verboseColor, "", a...)
+//}
+//
+//func (logger *DefaultLogger) Critical(a ...interface{}) {
+//	logger.printWithTime(nil, "", a...)
+//}
+//
+//func (logger *DefaultLogger) Infof(template string, a ...interface{}) {
+//	logger.printWithTime(nil, template, a...)
+//}
+//
+//func (logger *DefaultLogger) Debugf(template string, a ...interface{}) {
+//	logger.printWithTime(logger.debugColor, template, a...)
+//}
+//
+//func (logger *DefaultLogger) Warnf(template string, a ...interface{}) {
+//	logger.printWithTime(logger.warningColor, template, a...)
+//}
+//
+//func (logger *DefaultLogger) Errorf(template string, a ...interface{}) {
+//	logger.printWithTime(logger.errorColor, template, a...)
+//}
+//
+//func (logger *DefaultLogger) Verbosef(template string, a ...interface{}) {
+//	logger.printWithTime(logger.verboseColor, template, a...)
+//}
+//
+//func (logger *DefaultLogger) printWithTime(c *color.Color, template string, a ...interface{}) {
+//	now := time.Now().Format("15:04:05.00")
+//	msg := getMessage(template, a...)
+//	if c == nil {
+//		fmt.Println(now, msg)
+//	} else {
+//		c.Println(now, msg)
+//	}
+//}
+//
 func getMessage(template string, fmtArgs ...interface{}) string {
 	if len(fmtArgs) == 0 {
 		return template
