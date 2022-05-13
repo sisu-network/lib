472b80d0d606457197bcec9fd3270c6518abbb31
Benja Heart <510036+visopsys@users.noreply.github.com>
2022-05-12 17:42:42 -0700

    Add more ETH chains (#7)
    * Rename ropsten + binance
    
    * Add goerli testnet
    
    * Add arbitrum-testnet
    
    Co-authored-by: Benja Heart <visopsys@users.noreply.github.com>

--- chain/eth.go
+++ chain/eth.go
@@ -9,14 +9,16 @@ import (
 
 var (
 	ETH_CHAINS = map[string]bool{
-		"eth":                 true,
-		"eth-ropsten":         true,
-		"eth-binance-testnet": true,
-		"ganache1":            true,
-		"ganache2":            true,
-		"fantom-testnet":      true,
-		"polygon-testnet":     true,
-		"xdai":                true,
+		"eth":              true,
+		"ropsten-testnet":  true,
+		"goerli-testnet":   true,
+		"binance-testnet":  true,
+		"ganache1":         true,
+		"ganache2":         true,
+		"fantom-testnet":   true,
+		"polygon-testnet":  true,
+		"xdai":             true,
+		"arbitrum-testnet": true,
 	}
 	ethSigners map[string]etypes.Signer
 )
@@ -25,22 +27,26 @@ func initSigners() {
 	ethSigners = make(map[string]etypes.Signer)
 
 	ethSigners["eth"] = etypes.NewEIP2930Signer(GetChainIntFromId("eth"))
-	ethSigners["eth-ropsten"] = etypes.NewEIP2930Signer(GetChainIntFromId("eth-ropsten"))
-	ethSigners["eth-binance-testnet"] = etypes.NewEIP2930Signer(GetChainIntFromId("eth-binance-testnet"))
+	ethSigners["ropsten-testnet"] = etypes.NewEIP2930Signer(GetChainIntFromId("ropsten-testnet"))
+	ethSigners["goerli-testnet"] = etypes.NewEIP2930Signer(GetChainIntFromId("goerli-testnet"))
+	ethSigners["binance-testnet"] = etypes.NewEIP2930Signer(GetChainIntFromId("binance-testnet"))
 	ethSigners["ganache1"] = etypes.NewEIP2930Signer(GetChainIntFromId("ganache1"))
 	ethSigners["ganache2"] = etypes.NewEIP2930Signer(GetChainIntFromId("ganache2"))
 	ethSigners["fantom-testnet"] = etypes.NewEIP2930Signer(GetChainIntFromId("fantom-testnet"))
 	ethSigners["xdai"] = etypes.NewEIP2930Signer(GetChainIntFromId("xdai"))
 	ethSigners["polygon-testnet"] = etypes.NewEIP2930Signer(GetChainIntFromId("polygon-testnet"))
+	ethSigners["arbitrum-testnet"] = etypes.NewEIP2930Signer(GetChainIntFromId("arbitrum-testnet"))
 }
 
 func GetChainIntFromId(chain string) *big.Int {
 	switch chain {
 	case "eth":
 		return big.NewInt(1)
-	case "eth-ropsten":
+	case "ropsten-testnet":
 		return big.NewInt(3)
-	case "eth-binance-testnet":
+	case "goerli-testnet":
+		return big.NewInt(420)
+	case "binance-testnet":
 		return big.NewInt(97)
 	case "xdai":
 		return big.NewInt(100)
@@ -52,6 +58,8 @@ func GetChainIntFromId(chain string) *big.Int {
 		return big.NewInt(4002)
 	case "polygon-testnet":
 		return big.NewInt(80001)
+	case "arbitrum-testnet":
+		return big.NewInt(421611)
 	default:
 		log.Error("unknown chain:", chain)
 		return nil
@@ -60,7 +68,8 @@ func GetChainIntFromId(chain string) *big.Int {
 
 func IsETHBasedChain(chain string) bool {
 	switch chain {
-	case "eth", "eth-ropsten", "eth-binance-testnet", "ganache1", "ganache2", "fantom-testnet", "polygon-testnet", "xdai":
+	case "eth", "ropsten-testnet", "goerli-testnet", "binance-testnet", "ganache1", "ganache2",
+		"fantom-testnet", "polygon-testnet", "xdai", "arbitrum-testnet":
 		return true
 	}
 
